3eeb10704711f3c93394508b0bf40523ff5ace02
waszo <kawaaramatofonn2934@gmail.com>
2024-07-31 18:37:43 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'reservations/index'
+  get 'rooms/index'
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+    registrations:  'users/registrations'
+  }
+devise_scope :user do
+    get '/users/profile/edit', to: 'users/registrations#edit_profile', as: 'edit_user_profile'
+    put '/users/profile/edit', to: 'users#update_profile' 
+    patch '/users/profile/edit', to: 'users#update_profile'
 
-  get 'users/index'
-  get 'users/show'
+  end
+  root 'home#top'
+  get 'users/profile', to: 'users#profile', as: 'user_profile'
+  get 'users/account', to: 'users#account', as: 'user_acount'
+  put '/users/:id/update', to: 'users#update', as: 'update_user'
+  get 'rooms/own', to: 'rooms#own' ,as:'room_own'
+  get 'rooms/:id/edit', to: 'rooms#edit'
+  post 'rooms/:id/update', to: 'rooms#update'
+  
+  #get 'reservations/confirm' ,to: 'reservations#confirm'
+  get 'reservations/:id/edit', to: 'reservations#edit'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "posts" => "posts#index"
-  resources :posts
-  post "posts/:id/destroy"=>"posts#destroy"
-  get "posts/:id/destroy"=>"posts#destroy"
-end
+  post 'reservations/confirm'
+nd
