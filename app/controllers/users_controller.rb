aa54cf65e2adaff09b1e0fa67a1aed4233ed5a11
waszo <kawaaramatofonn2934@gmail.com>
2024-07-31 18:11:11 +0900

    コントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def index
+    def index
+      @test = 'テスト'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'users/index'
+  get 'users/show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "posts" => "posts#index"
   resources :posts
