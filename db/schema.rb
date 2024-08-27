8ee6202f5e53b6cc0bfaf8896b7d6babc7bfc7dc
waszo <kawaaramatofonn2934@gmail.com>
2024-08-27 18:47:15 +0900

    DB再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_26_155034) do
+ActiveRecord::Schema.define(version: 2024_08_27_094516) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
